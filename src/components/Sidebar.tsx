900cb99ffe5d3407d8c62c515260abc0a37a9b6e
Esi're <esiri92@gmail.com>
2022-03-24 00:49:16 +0100

    Add custom css.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
   return (
     <div className="fixed hidden h-full flex-col items-center p-2 sm:flex xl:w-[340px] xl:items-start">
       <div className="hover-animation flex h-14 w-14 items-center justify-center p-0 xl:ml-24">
-        <Image src="/images/twitter-icon-svg-28.png" width={30} height={30} />
+        <Image src="/images/twitter-icon-svg-28.png" width={30} height={30} alt="Logo" />
       </div>
     </div>
   )
