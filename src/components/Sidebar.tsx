e16f908f8af3baa0b645d8eb1bb4173b3d5aeff1
Esi're <esiri92@gmail.com>
2022-03-24 22:39:22 +0100

    Complete Sidebar.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import {
   BookmarkIcon,
   ClipboardListIcon,
   DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
   HashtagIcon,
   InboxIcon,
   UserIcon,
@@ -31,6 +32,21 @@ const Sidebar = () => {
         <SidebarLink text="Lists" Icon={ClipboardListIcon} />
         <SidebarLink text="Profile" Icon={UserIcon} />
         <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+      <button className="ml-auto hidden h-[52px] w-56 rounded-full bg-[#1D9BF0] text-lg font-bold text-white shadow hover:bg-[#1A8CD8] xl:inline">
+        Tweet
+      </button>
+      <div className="hover-animation mt-auto flex items-center justify-center text-[#D9D9D9] xl:ml-auto xl:-mr-5">
+        <img
+          src="https://lh3.googleusercontent.com/ogw/ADea4I5fb1DvkBz1-Ig4DTuqSEsyzMK6C0haJu38BXR7Hw=s32-c-mo"
+          alt="Esirei"
+          className="h-10 w-10 rounded-full xl:mr-2.5"
+        />
+        <div className="hidden leading-5 xl:inline">
+          <h4 className="font-bold">Esi;re</h4>
+          <p className="text-[#6E767D]">@Esi_rei</p>
+        </div>
+        <DotsHorizontalIcon className="ml-10 hidden h-5 xl:inline" />
       </div>
     </div>
   )
